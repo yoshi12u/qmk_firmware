31cc8ef5e37249ef76b65b234cc54db6ecee621f
yoshi12u <ysmsuehr@gmail.com>
2025-08-12 00:56:42 +0900

    fix(altair): swap inner index finger key positions in custom keymap
    Reorganize inner index finger column keys for improved ergonomics.

--- keyboards/ai03/altair/keymaps/custom/keymap.c
+++ keyboards/ai03/altair/keymaps/custom/keymap.c
@@ -36,9 +36,9 @@ bool process_detected_host_os_user(os_variant_t os) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TILD, KC_BSLS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        TAB_BELOW, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_BTN4, KC_LBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BTN5, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BTN4, KC_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+        TAB_BELOW, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_BTN5, KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_TILD, KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
                                    KC_LALT, EISU_LT, KC_SPC,  KC_LGUI, MO(_NAVIGATION),  KC_SPC,  KANA_L1, KC_ENT
     ),
 
