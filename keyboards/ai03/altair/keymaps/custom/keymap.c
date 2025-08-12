7439b99c56db2eedd5d2f077c06fbb966d897dcd
yoshi12u <ysmsuehr@gmail.com>
2025-08-13 08:07:21 +0900

    refactor: remove NUMPAD layer and reorganize layer assignments

--- keyboards/ai03/altair/keymaps/custom/keymap.c
+++ keyboards/ai03/altair/keymaps/custom/keymap.c
@@ -11,14 +11,13 @@ enum custom_keycodes {
 // ---------- Layers ----------
 enum layers {
     _BASE = 0,
-    _NUMPAD,      // Layer 1: Numpad / calculator
     _NAVIGATION,  // Layer 2: Navigation and arrows
-    _EMACS,       // Layer 4: Emacs-like cursor/editing (Windows via TAB_BELOW)
-    _MODIFIER     // Layer 5: Modifier layer for EISU hold
+    _EMACS,       // Layer 3: Emacs-like cursor/editing (Windows via TAB_BELOW)
+    _MODIFIER     // Layer 4: Modifier layer for EISU hold
 };
 
-// Built-in Layer-Tap for Kana: tap = KC_LNG1, hold = _NUMPAD
-#define KANA_L1 LT(_NUMPAD, KC_LNG1)
+// Built-in Layer-Tap for Kana: tap = KC_LNG1, hold = _NAVIGATION
+#define KANA_L1 LT(_NAVIGATION, KC_LNG1)
 
 // Built-in Layer-Tap for Eisu: tap = KC_LNG2, hold = _MODIFIER
 #define EISU_LT LT(_MODIFIER, KC_LNG2)
@@ -35,26 +34,19 @@ bool process_detected_host_os_user(os_variant_t os) {
 // ---------- Keymaps ----------
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BTN4, KC_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        TAB_BELOW, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_BTN5, KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PPLS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LPRN, KC_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+        TAB_BELOW, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_RPRN, KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_TILD, KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
-                                   KC_LALT, EISU_LT, KC_SPC,  KC_LGUI, MO(_NAVIGATION),  KC_SPC,  KANA_L1, KC_ENT
+                                   KC_LGUI, KC_LALT, EISU_LT, KC_SPC,  KC_SPC,  KANA_L1, KC_ENT,  KC_BTN2
     ),
 
-    [_NUMPAD] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_SLSH,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5  , KC_F6  , _______, KC_RPRN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_MINS,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_EQL,  _______,
-                                   _______, _______, _______, _______, _______, _______, _______, _______
-    ),
 
     [_NAVIGATION] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PWR,
+        _______, _______, _______, _______, _______, _______, _______, KC_BTN4, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_BTN5, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                    _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
