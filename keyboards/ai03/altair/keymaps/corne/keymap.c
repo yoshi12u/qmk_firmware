9d4f0d235aac76b9e7b22946857c5dc6d8459bd1
yoshi12u <ysmsuehr@gmail.com>
2025-08-17 15:31:43 +0900

    feat(keymap): reorganize altair corne layout with improved key positioning
    - Move LGUI from thumb cluster to middle key position
    - Replace LGUI in thumb with BSPC for better ergonomics
    - Add mouse buttons (BTN4/BTN5) to NUMBER layer
    - Reorganize NUMBER layer with arithmetic operators on left side
    - Move QK_BOOT to top-left position in FUNCTION layer
    - Replace KC_BSPC and KC_DEL with KC_DEL in thumb cluster

--- keyboards/ai03/altair/keymaps/corne/keymap.c
+++ keyboards/ai03/altair/keymaps/corne/keymap.c
@@ -154,10 +154,10 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_TAB,  Q_ESC,   KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC, US_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    US_MINS,
-        TAB_BELOW, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL,  US_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    US_SCLN, US_QUOT,
+        KC_TAB,  Q_ESC,   KC_W,    KC_E,    KC_R,    KC_T,    _______, US_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    US_MINS,
+        TAB_BELOW, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_LGUI, US_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    US_SCLN, US_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, KC_N,    KC_M,    US_COMM, US_DOT,  US_SLSH, KC_ENT,
-                                   _______, KC_LALT, EISU_LT, KC_LGUI, FUNC,    KANA_L1, NAV,     _______
+                                   _______, KC_LALT, EISU_LT, KC_BSPC, FUNC,    KANA_L1, NAV,     _______
     ),
 
     [_NAVIGATION] = LAYOUT(
@@ -178,16 +178,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUMBER] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        US_TILD, US_EXLM, US_AT,   US_HASH, US_DLR,  US_PERC, _______, _______, US_CIRC, US_AMPR, US_ASTR, US_LPRN, US_RPRN, US_PLUS,
-        US_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    US_EQL,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, US_BSLS, US_PIPE,
-                                   _______, _______, _______, _______, _______, _______, _______, _______
+        US_TILD, US_EXLM, US_AT,   US_HASH, US_DLR,  US_PERC, _______, KC_BTN4, US_CIRC, US_AMPR, US_ASTR, US_LPRN, US_RPRN, US_PLUS,
+        US_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, KC_BTN5, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    US_EQL,
+        _______, US_PLUS, US_MINS, US_ASTR, US_SLSH, US_EQL, _______, _______, _______, _______,  US_LBRC, US_RBRC, US_BSLS, US_PIPE,
+                                   _______, _______, _______, KC_DEL,  _______, _______, _______, _______
     ),
 
     [_FUNCTION] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PWR,
+        QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PWR,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                    _______, _______, _______, _______, _______, _______, _______, _______
     ),
